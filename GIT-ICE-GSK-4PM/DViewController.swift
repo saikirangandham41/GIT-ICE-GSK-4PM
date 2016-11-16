68c9c2442c203fb2b43409f865f5d3f2df82d8d0
Student <s525756@nw38334.nwmissouri.edu>
2016-11-16 16:14:02 -0600

    i have added d view controller

---
+++ GIT-ICE-GSK-4PM/DViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DViewController.swift
+//  GIT-ICE-GSK-4PM
+//
+//  Created by Pothamsetti,Krishna Jyothi Swaroop Reddy on 11/16/16.
+//  Copyright © 2016 Gandham,Sai Kiran. All rights reserved.
+//
+
+import UIKit
+
+class DViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
